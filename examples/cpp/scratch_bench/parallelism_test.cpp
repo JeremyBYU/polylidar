3c3065fc769a1e6bab1b996ee7f20ac61c3fbebd
Jeremy Castagno <jeremybyu@gmail.com>
2020-02-14 12:01:20 -0500

    Begginning benchmark tests

---
+++ examples/cpp/scratch_bench/parallelism_test.cpp
@@ -0,0 +1,14 @@
+/**
+    simple.cpp
+    Purpose: Example of using polylidar in C++
+    @author Jeremy Castagno
+    @version 05/20/19 
+*/
+#include <iostream>
+#include <sstream>      // std::istringstream
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iomanip>    
+
+
