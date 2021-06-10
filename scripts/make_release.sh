095cbec44e646c5ad525941ceca5dbaec3bbf24d
Jeremy Castagno <JeremyBYU@gmail.com>
2021-06-10 18:10:27 -0400

    Update Documentation

--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+# exit when any command fails
+set -e
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
 python scripts/manage_versions.py --bump patch
-pip install .
+pip install . --use-feature=in-tree-build
 cd src_docs && python make_docs.py && cd ..
 git status
 
@@ -14,4 +22,3 @@ then
     python scripts/manage_versions.py --tag
     echo -e "Dont forget to push:\n \t git push origin dev --tags"
 fi
-
